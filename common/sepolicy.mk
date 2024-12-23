062f161fc5938fa0cb8fb0f1be20553db9453f3a
Vhmit <viktor.9630@protonmail.com>
2024-12-23 17:08:30 -0300

    Revert "sepolicy: Drop Lineage Trust policy"
    This reverts commit 5ba6b67c9b88349dce563a7a6ad38ce7fb5f5551.

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -23,7 +23,8 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/dynamic
+    device/arrow/sepolicy/common/dynamic \
+    device/arrow/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/arrow/sepolicy/common/dynamic \
